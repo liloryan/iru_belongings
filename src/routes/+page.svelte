f441062c1ec67370065dc7a04dbd9e1f6c9df862
liloryan <liryan@hmc.edu>
2023-07-19 13:06:25 -0700

    create delete button; functional but no ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,21 @@
 		/>
 	</label>
 </form>
+<main>
+    <div class="container">
+      <h1 class="app-title">todos</h1>
+      <ul class="Patient Belongings"></ul>
+      <div class="empty-state">
+        <h2 class="empty-state__title">Add your first item to room</h2>
+        <p class="empty-state__description">Add a new item here.</p>
+      </div>
+      <form>
+        <input class="js-belongings-input" type="text" aria-label="Enter a new item to room" placeholder="E.g. Build a web app" />
+      </form>
+    </div>
+  </main>
 -->
+
 <h1>Room Management<br>
 <br> </h1>
 <script>
@@ -29,21 +43,29 @@ function addItem() {
   roomItems = [...roomItems, item];
   newItem = '';
 }
+
+function toggleDone(id) {
+  const index = roomItems.findIndex(item => item.id === Number(id));
+  roomItems[index].checked = !roomItems[index].checked;
+}
+
+function deleteItem(id) {
+  roomItems = roomItems.filter(item => item.id !== Number(id));
+}
+
 </script>
 
 <form on:submit|preventDefault={addItem}>
   <input class="js-item-input" type="text" aria-label="Enter a new item to room" placeholder="enter item here" bind:value={newItem}>
 </form>
 
-<ul class = "item-list">
+<ul class="item-list">
   {#each roomItems as item (item.id)}
-    <li class = "room-item">
-      <input id = {item.id} type = "checkbox" />
-      <label for {item.id} class = "tick"> </label>
+    <li class="room-item">
+      <input id={item.id} type = "checkbox" />
+      <label for={item.id} class = "tick"> </label>
       <span> {item.text}</span>
-      <button type = "delete-item">
-        <svg><use href = "#delete.svg"></use></svg>
-      </button>
+      <button class="btn btn-active" on:click={() => deleteItem(item.id)}>delete</button>
     </li>
   {/each}
 </ul>
